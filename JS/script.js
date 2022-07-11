95e423f923cc772a62fadeb58b114519da191356
Your Name <ishwarp@smbxl.com>
2022-07-11 15:50:21 +0530

    C-0

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("#calendar").evoCalendar({
+    theme: "Orange Coral",
+    // calendarEvents: [
+    //   {
+    //     id: "event1", // Event's id (required, for removing event)
+    //     description: "Lorem ipsum dolor sit amet..", // Description of event (optional)
+    //     badge: "1-day event", // Event badge (optional)
+    //     date: "July/11/2022", // Date of event
+    //     type: "event", // Type of event (event|holiday|birthday)
+    //     color: "#00d100", // Event custom color (optional)
+    //     everyYear: true, // Event is every year (optional)
+    //   },
+    // ],
+  });
+});
